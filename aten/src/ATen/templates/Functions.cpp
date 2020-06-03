c4586e315c37575f4dd04e67859cffdbc4eef8bd
Edward Z. Yang <ezyang@fb.com>
2020-06-03 07:08:09 -0700

    Un-inline Functions.h into Functions.cpp
    In my unscientific testing, this reduces startup time by 50% on gcc 8.3.
    That's a big fucking deal.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    ghstack-source-id: 33658894f0f6d1ddd3be217e1913dfbdef97dc2e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39446

---
+++ aten/src/ATen/templates/Functions.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+// ${generated_comment}
+
+#include <ATen/Functions.h>
+
+#include <ATen/core/dispatch/Dispatcher.h>
+#include <ATen/TypeDefault.h>
+#include <ATen/CPUType.h>
+#include <ATen/QuantizedCPUType.h>
+
+namespace at {
+
+using native::tensor;
+
+${function_definitions}
+
+}
